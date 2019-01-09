05f33962e6860519d7212f3d6fbe163e53d90ccb
Clément Aucoin <c.aucoin@protonmail.com>
2019-01-09 14:55:46 +0100

    add sign in

--- src/main/java/com/orb/userservice/web/controller/UserController.java
+++ src/main/java/com/orb/userservice/web/controller/UserController.java
@@ -2,29 +2,22 @@ package com.orb.userservice.web.controller;
 
 import com.orb.userservice.dao.UserDao;
 import com.orb.userservice.model.User;
-import com.orb.userservice.web.exceptions.UserAlreadyExists;
 import com.orb.userservice.web.exceptions.UserNotFoundException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.servlet.ServletException;
-import java.net.URI;
-import java.util.Date;
 import java.util.List;
 
 @Api(description = "API pour les opérations CRUD sur les utilisateurs")
 @RestController
 @RequestMapping(value = "/user")
-public class UserController {
+class UserController {
 
     @Autowired
     private UserDao _userDao;
+
 
     @ApiOperation(value = "Recupere la liste de tous les utilisateurs")
     @GetMapping(value = "/")
@@ -55,29 +48,6 @@ public class UserController {
         return _userDao.findByMail(mail);
     }
 
-    @ApiOperation(value = "Ajout d'un utilisateur")
-    @PostMapping(value = "/")
-    public ResponseEntity<User> create(@RequestBody User user) throws UserAlreadyExists {
-
-        if(_userDao.findByMail(user.getMail()) != null)
-            throw new UserAlreadyExists("L'adresse mail est déjà utilisée pour un autre compte.");
-        if(_userDao.findByPseudo(user.getPseudo()) != null)
-            throw new UserAlreadyExists("Ce pseudo existe déjà pour un autre compte");
-
-        User userAdded = _userDao.save(user);
-
-        if(userAdded == null)
-            return ResponseEntity.noContent().build();
-
-        URI loc = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(userAdded.getId())
-                .toUri();
-
-        return ResponseEntity.created(loc).build();
-    }
-
     @ApiOperation(value = "Mise à jour d'un utilisateur")
     @PutMapping(value = "/")
     public User update(@RequestBody User user){
@@ -90,37 +60,15 @@ public class UserController {
         _userDao.delete(user);
     }
 
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@RequestBody User login) throws ServletException {
 
-        String jwtToken = "";
-
-        if (login.getMail() == null || login.getPassword() == null) {
-            throw new ServletException("Please fill in username and password");
-        }
-
-        String email = login.getMail();
-        String password = login.getPassword();
-
-        User user = _userDao.findByMail(email);
-
-        if (user == null) {
-            throw new ServletException("User email not found.");
-        }
-
-        String pwd = user.getPassword();
-
-        if (!password.equals(pwd)) {
-            throw new ServletException("Invalid login. Please check your name and password.");
-        }
-
-        jwtToken = Jwts.builder().setSubject(email).claim("roles", "user").setIssuedAt(new Date())
-                .signWith(SignatureAlgorithm.HS256, "secretkey").compact();
-
-        return jwtToken;
+    @ApiOperation(value = "Connexion d'un utilisateur")
+    @PostMapping(value = "/logine")
+    public boolean logine(@RequestParam String pseudo, @RequestParam String password) throws UserNotFoundException {
+        User user = getByPseudo(pseudo);
+        if(user == null)
+            throw new UserNotFoundException("Utilisateur non existant");
+        if(!user.getPassword().equals(password))
+            return false;
+        return true;
     }
-
-    //@ApiOperation(value = "Déconnexion d'un utilisateur")
-    //@PostMapping(value= "/logout")
-
 }
